a73acae21c41f1629b557924c274b1cb844f94be
Sabaniki <sawada13@mccc.jp>
2020-05-15 18:13:15 +0900

    最低限

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MembersPageComponent} from './members-page/members-page/members-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'members', component: MembersPageComponent},
+  {path: '', redirectTo: '/members', pathMatch: 'prefix'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GroupPageComponent } from './group-page/group-page/group-page.component';
+import { GroupCardComponent } from './group-page/group-card/group-card.component';
+import { MembersPageComponent } from './members-page/members-page/members-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupPageComponent,
+    GroupCardComponent,
+    MembersPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
